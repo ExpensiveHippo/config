025fdb4dafc139ab1d67c4c14bc415357f6f63ac
Ryan <s10261024@connect.np.edu.sg>
2024-12-29 15:15:45 +0800

    add set

---
+++ nvim/lua/config/set.lua
@@ -0,0 +1,18 @@
+local opt = vim.opt
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+
+opt.clipboard:append { "unnamed" }
+
+opt.nu = true
+opt.relativenumber = true
+
+opt.wrap = false
+
+opt.hls = false
+
+opt.undodir = os.getenv("HOMEPATH") .. "/.vim/undo"
